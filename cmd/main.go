e4e1b2e4fd169de6f4265d752f9306a51155e7bf
Florian Sylvain <floriansylvainpro@gmail.com>
2022-12-19 19:08:56 +0100

    Split main func into multiples subfunctions

--- cmd/main.go
+++ cmd/main.go
@@ -22,13 +22,12 @@ func initJWT() {
 	}
 }
 
-func initGin() {
-	r := gin.Default()
-	r.SetTrustedProxies([]string{"localhost"})
-
+func initBasicRoutes(r *gin.Engine) {
 	r.POST("/login", internal.AuthMiddleware.LoginHandler)
 	r.GET("/ping", internal.Ping)
+}
 
+func initArticlesRoutes(r *gin.Engine) {
 	articlesRouter := r.Group("/articles/")
 	articlesRouter.Use(internal.AuthMiddleware.MiddlewareFunc())
 
@@ -36,6 +35,19 @@ func initGin() {
 	articlesRouter.GET("/:id", internal.GetArticle)
 	articlesRouter.POST("/:id", internal.AddArticle)
 	articlesRouter.DELETE("/:id", internal.DeleteArticle)
+}
+
+func initGin() {
+	ginMode := os.Getenv("GIN_MODE")
+	if ginMode == "release" {
+		gin.SetMode(ginMode)
+	}
+
+	r := gin.Default()
+	r.SetTrustedProxies([]string{"localhost"})
+
+	initBasicRoutes(r)
+	initArticlesRoutes(r)
 
 	r.Run(":" + os.Getenv("API_PORT"))
 }
