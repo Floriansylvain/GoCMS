961fb7a4e07f4e121df960ee4b25df3114abed52
Florian Sylvain <floriansylvainpro@gmail.com>
2022-12-20 18:19:30 +0100

    Added CORS middleware in debug mode

--- cmd/main.go
+++ cmd/main.go
@@ -37,14 +37,28 @@ func initArticlesRoutes(r *gin.Engine) {
 	articlesRouter.DELETE("/:id", internal.DeleteArticleHandler)
 }
 
+func corsMiddleware(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+	c.Next()
+}
+
 func initGin() {
+	r := gin.Default()
+	r.SetTrustedProxies([]string{"localhost"})
+
 	ginMode := os.Getenv("GIN_MODE")
 	if ginMode == "release" {
 		gin.SetMode(ginMode)
+	} else {
+		r.Use(corsMiddleware)
 	}
-
-	r := gin.Default()
-	r.SetTrustedProxies([]string{"localhost"})
 
 	initBasicRoutes(r)
 	initArticlesRoutes(r)
