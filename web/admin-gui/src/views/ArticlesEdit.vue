a4fb809da4ac83512d166042444fd56649e50b6f
Florian Sylvain <floriansylvainpro@gmail.com>
2023-01-07 00:10:52 +0100

    replace spaces with tabs

--- web/admin-gui/src/views/ArticlesEdit.vue
+++ web/admin-gui/src/views/ArticlesEdit.vue
@@ -8,67 +8,67 @@ let article: Ref<Article | void> = ref()
 const editorData: Ref<string> = ref('')
 
 onMounted(async () => {
-    article.value = await getArticle(useRoute().params.articleID as string)
-    editorData.value = article.value.content.html
+	article.value = await getArticle(useRoute().params.articleID as string)
+	editorData.value = article.value.content.html
 })
 
 function abort() {
 }
 
 function saveContent() {
-    console.log(editorData.value)
+	console.log(editorData.value)
 }
 </script>
-
+	
 <template>
-    <div class="container">
-        <div id="editor">
-            <Editor tinymce-script-src="/tinymce/tinymce.min.js"
-                :init="{ promotion: false, language: 'fr_FR', resize: false, height: '100%', }"
-                :plugins="['link', 'codesample']"
-                toolbar="undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | codesample link"
-                v-model="editorData">
-            </Editor>
-        </div>
-        <aside class="buttons">
-            <button @click="saveContent()" class="button-primary">Enregistrer</button>
-            <button @click="abort()" class="button-secondary">Annuler</button>
-        </aside>
-    </div>
+	<div class="container">
+		<div id="editor">
+			<Editor tinymce-script-src="/tinymce/tinymce.min.js"
+				:init="{ promotion: false, language: 'fr_FR', resize: false, height: '100%', }"
+				:plugins="['link', 'codesample']"
+				toolbar="undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | codesample link"
+				v-model="editorData">
+			</Editor>
+		</div>
+		<aside class="buttons">
+			<button @click="saveContent()" class="button-primary">Enregistrer</button>
+			<button @click="abort()" class="button-secondary">Annuler</button>
+		</aside>
+	</div>
 </template>
 
 <style scoped>
 .container {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
 }
 
 #editor {
-    padding-top: 16px;
-    width: 100%;
-    height: 100%;
+	padding-top: 16px;
+	width: 100%;
+	height: 100%;
 }
 
 .tox-tinymce {
-    border-radius: 0 !important;
+	border-radius: 0 !important;
 }
 
 .buttons {
-    position: relative;
-    z-index: 1;
+	position: relative;
+	z-index: 1;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: right;
-    align-content: center;
-    gap: 16px;
+	display: flex;
+	flex-direction: column;
+	justify-content: right;
+	align-content: center;
+	gap: 16px;
 
-    width: fit-content;
-    padding: 32px 16px;
+	width: fit-content;
+	padding: 32px 16px;
 
-    box-shadow: #0002 0 0 10px;
+	box-shadow: #0002 0 0 10px;
 }
 </style>
