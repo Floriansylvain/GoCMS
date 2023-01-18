53b560d63de5352d920128aaecfb37c9d5a1f42a
Florian Sylvain <floriansylvainpro@gmail.com>
2023-01-18 20:04:12 +0100

    Added an anecdotal home page

--- web/admin-gui/src/views/Home.vue
+++ web/admin-gui/src/views/Home.vue
@@ -2,9 +2,19 @@
 </script>
 
 <template>
-    <p>home page</p>
+    <main>
+        <h1>Bienvenue.</h1>
+        <h2>Vous êtes sur la page d'accueuil de GohCMS.</h2>
+    </main>
 </template>
 
 <style scoped>
+main {
+    padding: 64px;
+    color: var(--neutral-light);
+}
 
+main h1 {
+    margin: 0;
+}
 </style>
